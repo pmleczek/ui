95618f450f6eaa59aa0ef529b01b8d821536d1e6
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2026-08-01 17:23:24 +0200

    chore: set up oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  ignorePatterns: ["*.config.ts", "node_modules"],
+  sortImports: {
+    newlinesBetween: true,
+    groups: [
+      ['side_effect_style', 'side_effect'],
+      'builtin',
+      ['type-external', 'value-external'],
+      [
+        'type-parent',
+        'type-sibling',
+        'type-index',
+        'value-parent',
+        'value-sibling',
+        'value-index',
+      ],
+      'style',
+      'unknown',
+    ],
+  },
+  sortPackageJson: true,
+});
